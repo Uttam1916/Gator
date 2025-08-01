745ab1575c5bd6936159d296fdbd5c03b997610b
Uttam <tired3120@gmail.com>
2025-08-02 00:30:30 +0530

    feat: add user registration with PostgreSQL, Goose migrations, and SQLC integration

--- main.go
+++ main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
+	"log"
 	"os"
+	"time"
 
 	"github.com/Uttam1916/Gator/internal/config"
+	"github.com/Uttam1916/Gator/internal/database"
+	"github.com/google/uuid"
+	_ "github.com/lib/pq"
 )
 
 type state struct {
 	configpointer *config.Config
+	db            *database.Queries
 }
 
 type command struct {
@@ -26,6 +34,21 @@ var cfg config.Config
 var comms commands
 
 func main() {
+	//initialize state
+	dbURL := "postgres://postgres:postgres@localhost:5432/gator?sslmode=disable"
+	conn, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal("could not connect to DB:", err)
+	}
+	dbQueries := database.New(conn)
+
+	cfg = config.Read()
+
+	ste := state{
+		db:            dbQueries,
+		configpointer: &cfg,
+	}
+
 	if len(os.Args) < 2 {
 		fmt.Println("not enough arguements")
 		os.Exit(1)
@@ -37,13 +60,10 @@ func main() {
 	comms = commands{
 		command_map: make(map[string]func(*state, command) error),
 	}
-	cfg = config.Read()
-	ste.configpointer = &cfg
-
 	//initialize command handlers
 	comms.register("login", handlerLogin)
-
-	err := comms.run(&ste, cmd)
+	comms.register("register", handlerRegister)
+	err = comms.run(&ste, cmd)
 	if err != nil {
 		fmt.Printf("error:%v \n", err)
 	}
@@ -67,10 +87,46 @@ func handlerLogin(s *state, c command) error {
 		return fmt.Errorf("login requires a username")
 	}
 	username := c.arguments[0]
-	err := s.configpointer.SetUser(username)
+	_, err := s.db.GetUserByName(context.Background(), username)
+	if err != nil {
+		fmt.Println("cant log into non existent user")
+		os.Exit(1)
+	}
+
+	err = s.configpointer.SetUser(username)
 	if err != nil {
 		return err
 	}
 	fmt.Printf("User set to %s\n", username)
+	return nil
+}
+
+func handlerRegister(s *state, c command) error {
+	if len(c.arguments) < 1 {
+		fmt.Println("register requires a username")
+		os.Exit(1)
+	}
+	//check if user already exists
+	_, err := s.db.GetUserByName(context.Background(), c.arguments[0])
+	if err == nil {
+		fmt.Println("user already exists")
+		os.Exit(1)
+	}
+	usertobecreated := database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      c.arguments[0],
+	}
+	_, err = s.db.CreateUser(context.Background(), usertobecreated)
+	if err != nil {
+		return fmt.Errorf("couldnt create new user: ", err)
+	}
+	err = s.configpointer.SetUser(c.arguments[0])
+	if err != nil {
+		return fmt.Errorf("couldnt set new user: ", err)
+	}
+
+	fmt.Printf("User '%s' registered successfully\n", c.arguments[0])
 	return nil
 }
---
+++ sql/schema/001_users.sql
@@ -0,0 +1,12 @@
+-- +goose UP
+
+CREATE TABLE users (
+    id UUID PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    name TEXT UNIQUE NOT NULL
+);
+
+-- +goose DOWN
+
+DROP TABLE users;
